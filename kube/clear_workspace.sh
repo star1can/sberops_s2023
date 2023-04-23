0815528442c3c02f8d7c1d3f8f66c1a67db9f819
Dmitrii <stardima08@yandex.ru>
2023-04-24 01:51:49 +0300

    better script

--- kube/clear_workspace.sh
+++ kube/clear_workspace.sh
@@ -6,5 +6,12 @@ kubectl delete vs pokeapi-through-egress-gateway pokemon-service-ingress
 kubectl delete cm nginx-cm pokemon-service-cm
 kubectl delete svc pokemon-service-cip nginx-cip
 kubectl delete gateway pokeapi-egressgateway pokemon-service-gateway
+kubectl label namespace default istio-injection=disabled
+
+cd istio-1.17.2
+export PATH=$PWD/bin:$PATH
+istioctl manifest generate --set profile=demo | kubectl delete -f -
+cd ..
+
 ../docker/clear-image.sh
 rm -fr istio-1.17.2
