4a43e8c437cfe1763e2d5dba9c6098a92ef57a2a
Dmitrii <stardima08@yandex.ru>
2023-04-24 01:33:40 +0300

    better script

--- kube/clear_workspace.sh
+++ kube/clear_workspace.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-minikube delete
+kubectl delete deployment pokemon-service-deployment nginx-deployment
+kubectl delete se pokeapi
+kubectl delete vs pokeapi-through-egress-gateway pokemon-service-ingress
+kubectl delete cm nginx-cm pokemon-service-cm
+kubectl delete svc pokemon-service-cip nginx-cip
+kubectl delete gateway pokeapi-egressgateway pokemon-service-gateway
 ../docker/clear-image.sh
 rm -fr istio-1.17.2
