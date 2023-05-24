40d56dc70b09285e592a75e94f7e813c1847b113
Dmitrii <stardima08@yandex.ru>
2023-05-24 23:03:24 +0300

    HW2 add script for test

--- kube/run.sh
+++ kube/run.sh
@@ -15,6 +15,7 @@ istioctl install --set profile=demo --set meshConfig.outboundTrafficPolicy.mode=
 cd ..
 
 # create certs
+<<<<<<< Updated upstream
 mkdir certs
 openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=pokemons Inc./CN=pokemons.com' -keyout certs/pokemons.com.key -out certs/pokemons.com.crt
 openssl req -out certs/mipt.pokemons.com.csr -newkey rsa:2048 -nodes -keyout certs/mipt.pokemons.com.key -subj "/CN=mipt.pokemons.com/O=mipt organization"
@@ -24,6 +25,28 @@ kubectl create -n istio-system secret generic pokemons-secret \
   --from-file=tls.key=certs/mipt.pokemons.com.key \
   --from-file=tls.crt=certs/mipt.pokemons.com.crt \
   --from-file=ca.crt=certs/pokemons.com.crt
+=======
+
+mkdir certs
+openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=pokemons Inc./CN=pokemons.com' -keyout certs/pokemons.com.key -out certs/pokemons.com.crt
+
+openssl req -out certs/pokeapi.mesh-external.svc.cluster.local.csr -newkey rsa:2048 -nodes -keyout certs/pokeapi.mesh-external.svc.cluster.local.key -subj "/CN=pokeapi.mesh-external.svc.cluster.local/O=baykar organization"
+openssl x509 -req -sha256 -days 365 -CA certs/pokemons.com.crt -CAkey certs/pokemons.com.key -set_serial 0 -in certs/pokeapi.mesh-external.svc.cluster.local.csr -out certs/pokeapi.mesh-external.svc.cluster.local.crt
+
+openssl req -out certs/client.pokemons.com.csr -newkey rsa:2048 -nodes -keyout certs/client.pokemons.com.key -subj "/CN=client.pokemons.com/O=client organization"
+openssl x509 -req -sha256 -days 365 -CA certs/pokemons.com.crt -CAkey certs/pokemons.com.key -set_serial 1 -in certs/client.pokemons.com.csr -out certs/client.pokemons.com.crt
+
+
+kubectl create secret -n istio-system generic client-secret --from-file=tls.key=certs/client.pokemons.com.key \
+  --from-file=tls.crt=certs/client.pokemons.com.crt --from-file=ca.crt=certs/pokemons.com.crt
+
+
+kubectl create namespace mesh-external
+kubectl create -n mesh-external secret tls nginx-server-certs --key certs/pokeapi.mesh-external.svc.cluster.local.key --cert certs/pokeapi.mesh-external.svc.cluster.local.crt
+kubectl create -n mesh-external secret generic nginx-ca-certs --from-file=certs/pokemons.com.crt
+
+kubectl apply -f pokeapi/service.yml -f pokeapi/configmap/configmap.yml
+>>>>>>> Stashed changes
 
 # add sidecar proxies auto injection for any pod in ns default
 kubectl label namespace default istio-injection=enabled
